a2892656a445b9061e5537811a593ad1d379f5c2
mayurrohokale <mayurrohokale12345@gmail.com>
2024-05-05 12:35:56 +0530

    made resposinve login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,12 @@
 import { Link } from "react-router-dom";
 
 const Cstbutton = ({ text }) => {
-    return (
-      <button className="bg-[#2196F3] mt-3 text-white font-bold py-2 px-4  rounded">
-        {text}
-      </button>
-    );
-  };
-
-
+  return (
+    <button className="bg-[#2196F3] mt-3 text-white font-bold py-2 px-4 rounded">
+      {text}
+    </button>
+  );
+};
 
 const CustomInput = ({ label, type, placeholder, value, onChange }) => {
   return (
@@ -17,7 +15,7 @@ const CustomInput = ({ label, type, placeholder, value, onChange }) => {
         <label className="">{label}</label>
         <br />
         <input
-          className="border border-gray-300 hover:border-[#2196F3] rounded shadow-lg h-[30px]  w-[240px]  lg:w-[380px] max:w-[1536px] md:h-[40px] px-5 py-2"
+          className="border border-gray-300 hover:border-[#2196F3] rounded shadow-lg h-[30px] w-[240px] lg:w-[380px] max:w-[1536px] md:h-[40px] px-5 py-2"
           type={type}
           placeholder={placeholder}
           value={value}
@@ -30,22 +28,31 @@ const CustomInput = ({ label, type, placeholder, value, onChange }) => {
 
 export default function Login() {
   return (
-    <div className="container">
-      <h1>Login</h1>
-      <form action="/login" method="post">
+    <div className="container flex justify-center items-center h-screen p-4 ">
+      <div className="shadow-2xl border border-gray-400 hover:border-blue-500 p-8 rounded-md flex-col flex md:flex-row gap-8 ">
         <div>
-          <CustomInput label="Email" type="text" />
-          <CustomInput
-            label="Password"
-            type="password"
-            placeholder={"Enter your password"}
-          />
+          <h1 className="font-bold text-2xl">Login</h1>
+          <form action="/login" method="post">
+            <div>
+              <CustomInput label="Email" type="text" />
+              <CustomInput
+                label="Password"
+                type="password"
+                placeholder={"Enter your password"}
+              />
+            </div>
+            <Cstbutton text={"Login"} />
+          </form>
+          <p>
+            Not Registered? <Link to="register">Register Now!</Link>
+          </p>
         </div>
-        <Cstbutton text={"Login"} />
-        
-      </form>
-      <p>Not Registered? <Link to="register">Register Now!</Link></p>
-      {/* If user is logged in, display logout button */}
+
+        {/* If user is logged in, display logout button */}
+        <div className="max-w-[450px] ">
+          <img src="./images/img2.jpg" className="rounded-lg" alt="wg" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Home/Volentier.js
+++ src/components/Home/Volentier.js
@@ -1,5 +1,5 @@
 import { MdArrowOutward } from "react-icons/md";
-
+import { Link } from "react-router-dom";
 export default function Volentier() {
     return (
       <div className="bg-[#BBDEFB] " id="voulunteers">
@@ -66,10 +66,12 @@ export default function Volentier() {
           </div>
         </div>
         <div className="flex justify-center md:justify-end mx-16 mt-8 mb-4 p-2">
+          <Link to="login">
           <button className="bg-[#E91E63] p-2 text-white font-bold text-[15px] md:text-[25px] flex flex-row font-josiefin">
             APPLY NOW &ensp;
             <MdArrowOutward className=" items-center" />
           </button>
+          </Link>
         </div>
       </div>
     );
