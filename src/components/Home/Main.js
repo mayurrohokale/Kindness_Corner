400dbc655f38607b07da83e21d007712f95dfbce
mayurrohokale <mayurrohokale12345@gmail.com>
2024-05-07 01:10:41 +0530

    added slogans for images

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import Donateform from "./Donateform";
 import Campaign from "./Campaign/Campaign";
 import Volentier from "./Volentier";
-// import Votingform from "./Campaign/votingform";
 import Completedworks from "./Completedworks";
 import Blogs from "./Blogs";
 import Footer from "./Footer";
@@ -10,21 +9,20 @@ import Footer from "./Footer";
 const slideImages = [
   {
     url: "./images/img1.jpg",
-    caption: "We Belive in Transparency",
-    caption2:"",
+    caption: "We Believe in Transparency",
+    caption2: "Read more",
   },
   {
-    url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    caption: "Slide 2",
+    url: "./images/img3.jpg",
+    caption: "together We make difference",
   },
   {
-    url: "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    caption: "",
+    url: "./images/img4.jpg",
+    caption: "Helping hands, caring hearts.",
   },
 ];
 
 const Main = () => {
-
   const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
 
   React.useEffect(() => {
@@ -36,47 +34,45 @@ const Main = () => {
 
     return () => clearInterval(interval);
   }, []);
-  
+
   return (
     <div id="home">
-    <div className="flex flex-col
-     max-w-[1536px]">
-      <div className="relative w-full">
-        <div className="relative z-0 ">
-       
-          {
+      <div className="flex flex-col max-w-[1536px]">
+        <div className="relative w-full">
+          <div className="relative z-0">
             <img
-            src={slideImages[currentImageIndex].url}
-            alt={slideImages[currentImageIndex].caption}
-            className="w-full object-cover shadow-md lg:h-screen xl:h-[88vh]"
-          />
-          }
-        </div>
-        <div className=" absolute top-3 xl:top-8 right-3 sm:right-5 ">
-          <div className="hidden lg:flex  ">
-            <Donateform />
+              src={slideImages[currentImageIndex].url}
+              alt={slideImages[currentImageIndex].caption}
+              className="w-full object-cover shadow-md lg:h-screen xl:h-[88vh]"
+            />
+            <div className="absolute bottom-4 md:bottom-16 left-0 right-0 bg-black bg-opacity-10 text-white text-start py-2 px-8 md:px-12 text-[22px] md:text-[38px] font-bold font-josiefin">
+              {slideImages[currentImageIndex].caption}
+              {/* <p className="text-[10px] md:text-[20px]  text-black underline" >Read More!</p> */}
+            </div>
+          </div>
+          <div className="absolute top-3 xl:top-8 right-3 sm:right-5">
+            <div className="hidden lg:flex">
+              <Donateform />
+            </div>
           </div>
         </div>
+        <div className="lg:hidden w-full flex flex-col items-center z-10 px-3 py-2 lg:px-3 lg:py-5">
+          <Donateform />
+        </div>
+        <Volentier />
+        <div className="mt-8">
+          <Campaign />
+        </div>
+        <div>
+          <Completedworks />
+        </div>
+        <div>
+          <Blogs />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <div className="lg:hidden w-full flex flex-col items-center z-10 px-3 py-2 lg:px-3 lg:py-5 ">
-        <Donateform />
-      </div>
-      <Volentier />
-      <div className="mt-8">
-       <Campaign/>
-      </div>
-      <div>
-      <Completedworks/>
-      </div>
-      <div>
-        <Blogs/>
-      </div>
-      <div>
-        <Footer/>
-      </div>
-     
-    </div>
-    
     </div>
   );
 };
