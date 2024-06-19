3f9002043ee84d87f548331b6bb1b0945eba71b7
mayurrohokale <mayurrohokale12345@gmail.com>
2024-06-19 23:39:22 +0530

    font changes

--- src/components/Home/Blogs/Blogsform.js
+++ src/components/Home/Blogs/Blogsform.js
@@ -41,11 +41,11 @@ export default function Blogsform() {
               <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-4">
                   {/* <img className="w-7 h-7 rounded-full" src="https://via.placeholder.com/150" alt="Author avatar" /> */}
-                  <span className="font-medium text-[11px] md:text-[18px] dark:text-black">
+                  <span className="font-medium text-[14px] md:text-[18px] dark:text-black">
                     Author Name
                   </span>
                 </div>
-                <a href="#" className="inline-flex text-[11px] md:text-[18px]  items-center font-medium text-blue-600 dark:text-blue-700 hover:underline">
+                <a href="#" className="inline-flex text-[14px] md:text-[18px]  items-center font-medium text-blue-600 hover:underline">
                   Read more
                   <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </a>
