957a6d9d1c8ea889e78ff9fcb543b0ba04b586c3
mayurrohokale <mayurrohokale12345@gmail.com>
2024-08-26 21:22:22 +0530

    ChangedHeader position in Forgot Component

--- src/components/Auth/ForgotPass.js
+++ src/components/Auth/ForgotPass.js
@@ -30,25 +30,24 @@ const CustomInput = ({ label, type, placeholder, value, onChange }) => {
 export default function ForgotPassword() {
   return (
     <div className="h-screen flex justify-center items-center">
-    <div className="text-center w-[380px] h-[230px] lg:w-[600px] lg:h-[350px] shadow-xl rounded-xl px-4 flex flex-col justify-center hover:border hover:border-blue-500">
-      <h1 className="text-base lg:text-lg font-monserrat font-semibold mb-4 ">
-        Enter your Email to Reset Your Password!
-      </h1>
-      <div className="flex justify-center items-center gap-2">
-        <div>
-          <CustomInput
-            type="email"
-            placeholder="Enter Your Existing Email"
-            label="Email"
-          />
-          <Cstbutton text="Send" disabled={false} />
-        </div>
-        <div className="w-[300px] hidden lg:block">
-          <img src="./images/forgot.jpg" alt="forgot_image" />
+      <div className="text-center w-[380px] h-[230px] lg:w-[600px] lg:h-[350px] shadow-xl rounded-xl px-4 flex flex-col justify-center hover:border hover:border-blue-500">
+        <div className="flex justify-center items-center gap-2">
+          <div>
+            <h1 className="text-base lg:text-lg font-monserrat font-semibold mb-4 ">
+              Enter your Email to Reset Your Password!
+            </h1>
+            <CustomInput
+              type="email"
+              placeholder="Enter Your Existing Email"
+              label="Email"
+            />
+            <Cstbutton text="Send" disabled={false} />
+          </div>
+          <div className="w-[300px] hidden lg:block">
+            <img src="./images/forgot.jpg" alt="forgot_image" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  
   );
 }
